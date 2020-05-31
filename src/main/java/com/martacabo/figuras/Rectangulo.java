b028d36404f672402ed906879cd2076e1eae2abd
Marta Cabo <marta.cabo.alum@iescamp.es>
2020-05-31 14:21:01 +0200

    Creacion de la clase 'Rectangulo' y el metodo 'areaRectang'.

---
+++ src/main/java/com/martacabo/figuras/Rectangulo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.martacabo.figuras;
+
+/**
+ *
+ * @author marcabo
+ */
+public class Rectangulo {
+    private double lado1,lado2;
+
+    public Rectangulo(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public double areaRectang(){
+        double area;
+        area=lado1*lado2;
+        return area;
+    }
+    
+    
+    
+    
+    
+}
